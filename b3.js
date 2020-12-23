863806dc6982f16b4537ccac21ab0696ead2bc89
Alina <alina@Apples-MBP.lan>
2020-12-23 19:44:45 +0200

    init (start coding)

---
+++ b3.js
@@ -0,0 +1,11 @@
+'use strict'
+function calculationFunc(expression) {
+    
+}
+
+
+function runB3() {
+    let userExpression = prompt('Введите выражение, которое необходимо рассчитать');
+    let calculatedAnswer = calculationFunc(userExpression);
+    alert(calculatedAnswer);
+}
